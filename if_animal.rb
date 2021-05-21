6a46d606b751a236c553bbf6139811ec1f444436
Naru-hub <1208ny0324@gmail.com>
2021-05-21 02:58:38 +0000

    Initial Commit

---
+++ if_animal.rb
@@ -0,0 +1,17 @@
+#例題２
+#動物の種類によって、出力する鳴き声を変えるプログラム
+#cat :  meow
+#dog : bowwow
+#cow : moomoo
+#上記以外 : Not found.
+
+animal = 'horse'
+if animal == 'cat'
+  puts 'meow'
+elsif animal == 'dog'
+  puts 'bowwow'
+elsif animal == 'cow'
+  puts 'moomoo'
+else
+  puts 'Not found.'
+end
