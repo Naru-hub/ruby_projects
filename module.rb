81750ce34a3786bc11e456b041e95728c5157d92
Naru-hub <1208ny0324@gmail.com>
2021-05-28 14:04:16 +0000

    add class module

---
+++ module.rb
@@ -0,0 +1,17 @@
+#自転車の運転者に関するモジュールの作成
+#Runと出力するメソッド
+#Stopと出力するメソッド
+
+module Driver
+  def self.run
+    puts 'Run'
+  end
+  
+  def self.stop
+    puts 'Stop'
+  end
+end
+
+Driver.run
+Driver.stop
+
